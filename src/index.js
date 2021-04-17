fcf03a3723218d717b43544f4e427504f18e8666
Sam <samara.amanda01@gmail.com>
2021-04-16 20:11:09 -0400

    refactored render

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const endPoint = "http://localhost:3000/api/v1/cars"
 
 document.addEventListener('DOMContentLoaded', () => {
+    // fetch and load cars
     getCars()
 
     const createCarForm = document.querySelector("#create-car-form")
@@ -9,28 +10,37 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 function getCars() {
+    // get request
     fetch(endPoint)
     .then(response => response.json())
     .then(cars => {
         cars.data.forEach(car => {
-            //accessing the attributes of each individual object.
-            const carMarkup = `
-            <div data-id=${car.id}>
-                <img src=${car.attributes.image_url} height="250" width="350">
-                
-                <h2>${car.attributes.year} ${car.attributes.brand} ${car.attributes.model}</h2>
-
-                <p> ${"$" + car.attributes.price} </p>
-                <p>Contact Info: ${car.attributes.user.name}</p>
-                <p> ${car.attributes.user.email}</p>
-
-                <button>Edit</button>
-                <br><br>
-            </div>`;
-
-            document.querySelector('#car-container').innerHTML += carMarkup
+            //accessing the attributes of each individual object. and manipulating the DOM
+            render(car)
         })
     })
+}
+
+function render(car) {
+    const carMarkup = `
+        <div data-id=${car.id}>
+
+          <img src=${car.attributes.image_url} height="200" width="250">
+
+          <h3>${car.attributes.year} ${car.attributes.brand} ${car.attributes.model}</h3>
+
+          <p>${"$" + car.attributes.price}</p>
+
+          <p>Contact Info: ${car.attributes.user.name} </p>
+
+          <p> ${car.attributes.user.email}</p>
+
+          <button data-id=${car.id}>edit</button>
+        </div>
+        <br><br>`;
+
+        // maniputlating the DOM
+    document.querySelector('#car-container').innerHTML +=  carMarkup;
 }
 
 function createFormHandler(e) {
@@ -46,36 +56,20 @@ function createFormHandler(e) {
 }
 
 function postFetch(year, brand, model, price, image_url, user_id) {
-
+    // build my body object outside of my
     const bodyData = {year, brand, model, price, image_url, user_id}
 
-     fetch(endPoint, {
+    fetch(endPoint, {
         // POST request.. sends back the object to the API 
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(bodyData)
-      })
+    })
       .then(response => response.json())
       .then(car => {
-        const carData = car.data.attributes
+        const carData = car.data
         // render JSON response
-        const carMarkup = `
-        <div data-id=${car.id}>
-
-          <img src=${carData.image_url} height="200" width="250">
-
-          <h3>${carData.year} ${carData.brand} ${carData.model}</h3>
-
-          <p>${"$" + carData.price}</p>
-
-          <p>Contact Info: ${carData.user.name} </p>
-
-          <p> ${carData.user.email}</p>
-
-          <button data-id=${carData.id}>edit</button>
-        </div>
-        <br><br>`;
-
-        document.querySelector('#car-container').innerHTML +=  carMarkup;
-      })
+        render(carData)
+    })
 }
+
