778d9aa12e156a162d278f008ad48a6a3547d54d
Sam <samara.amanda01@gmail.com>
2021-04-20 00:16:03 -0400

    refactoring api code into separate file

--- src/index.js
+++ src/index.js
@@ -1,90 +1,25 @@
-const baseURL = "http://localhost:3000/api/v1/cars"
+const baseURL = "http://localhost:3000/api/v1/cars" 
 
-document.addEventListener('DOMContentLoaded', () => {
-    // fetch and load cars
-    getCars()
+const list = document.querySelector("#car-list")
+const form = document.getElementById('create-car-form')
 
-    // listen for submit event on form and handle data
-    const createCarForm = document.querySelector("#create-car-form")
-    createCarForm.addEventListener("submit", (e) => car.createCarForm(e))
-
-     // listen for the submit event of the edit form and handle the data
-    
-    document.querySelector('#update-car').addEventListener('submit', e => car.saveUpdatedCar(e))
-})
+const yearInput = document.getElementById('input-year')
+const brandInput = document.getElementById('input-brand')
+const modelInput = document.getElementById('input-model')
+const priceInput = document.getElementById('input-price')
+const userInput = document.getElementById('users')
+const imageUrlInput = document.getElementById('input-image-url')
 
 
-function getCars() {
-    // get request and renders cars
-    fetch(baseURL)
-    .then(response => response.json())
-    .then(cars => {
-        cars.data.forEach(car => {
-            //accessing the attributes of each individual object. and manipulating the DOM
-            let newCar = new Car(car, car.attributes)
-            newCar.attachToDom()
-            // dried up code, added the render call in the Car class file. 
-        })
-    })
-}
+form.addEventListener('submit', handleFormSubmit)
 
 
-function postFetch(car) {
-    // build my body object outside of my
-    const bodyData = {year, brand, model, price, image_url, user_id}
+function handleFormSubmit(e) {
+    e.preventDefault()
 
-    fetch(baseURL, {
-        // POST request.. sends back the object to the API 
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(bodyData)
-    })
-      .then(response => response.json())
-      .then(car => {
-        const carData = car.data
-        // render JSON response
-        let newCar = new Car(carData, carData.attributes)
-        
-        document.querySelector("#car-list").innerHTML += newCar.renderCar()
-    })
-}
+    CarApi.createCar()
+    form.reset()
+} 
 
 
-function patchCar(car) {
-    let {year, brand, model, price, image_url, user_id} = car
-    const carInfo = {year, brand, model, price, image_url, user_id}
-
-    fetch(`${baseURL}/${car.id}`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-      body: JSON.stringify(carInfo),
-    })
-      .then(res => res.json())
-      
-      // our backend responds with the updated syllabus instance represented as JSON
-      .then(json => {
-          car.renderCar()
-      })
-
-}
-
-function deleteCar(id) {
-    
-    fetch(`${baseURL}/${id}`, {
-
-        method: 'DELETE',
-        headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json"
-        }
-    })
-        .then(r => r.json())
-        .then(json => alert(json.message))
-        
-    
-}
-
-
+CarApi.getCars()  
