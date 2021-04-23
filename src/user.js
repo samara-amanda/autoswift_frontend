dbd7229304bb9e86b30e26187a8051b1a69db00a
Sam <samara.amanda01@gmail.com>
2021-04-23 14:05:18 -0400

    created user class

---
+++ src/user.js
@@ -0,0 +1,24 @@
+class User {
+    constructor(user, userAttributes) {
+        this.id = user.id
+        this.name = userAttributes.name
+        this.email = userAttributes.email
+
+        User.all.push(this)
+    }
+
+    attachToUserSelectDom() {
+        const option = document.createElement('option')
+        option.value  = this.id 
+        option.innerText = this.name
+        userSelectDom.prepend(option)
+    }
+
+    static createUserHandler(event) {
+        event.preventDefault()
+        const userInput = parseInt(document.querySelector('#users-select').value)
+        UserApi.postUserReq(userInput)
+    }
+}
+
+User.all = [];
