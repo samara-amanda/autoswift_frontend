e5804d05562f22ced2dc0ed34b8d6f8815dba1d0
Sam <samara.amanda01@gmail.com>
2021-05-09 22:34:27 -0400

    no change

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ const userUrl = "http://localhost:3000/api/v1/users"
 const container = document.querySelector("#car-container")
 const createCarForm = document.querySelector('#create-car-form')
 const userSelectDom = document.querySelector('#users-select')
+const editButton = document.querySelector('#edit-btn')
+
+createCarForm.addEventListener('submit', (event) => Car.createCarHandler(event))
 
 
-createCarForm.addEventListener('submit', (event) => Car.createCarHandler(event)) 
 
 UserApi.getUsersReq()
 
 CarApi.getCarsReq()
+
